0cc3d5ce386c671a5774c64e74a2ccbe46ec987b
Nikesh Prajapati <nprajapati@ilsnyc.om>
2021-11-29 19:25:16 -0600

    Greeter updated

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,18 +1,18 @@
 pragma solidity ^0.4.0;
 
 contract Greeter  {
-	string public yourName;  // data
+    string public yourName;  // data
 
 	/* This runs when the contract is executed */
-	function Greeter() public {
-		yourName = "World";
-	} 
+    function Greeter() public {
+        yourName = "World";
+    } 
 
-	function set(string name)public {
-		yourName = name;
-	}
+    function set(string name)public {
+        yourName = name;
+    }
 
-	function hello() constant public returns (string) {
-		return yourName;
-	}
+    function hello() constant public returns (string) {
+        return yourName;
+    }
 }
